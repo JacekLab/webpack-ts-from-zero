809c6436942bfb2a7ac5561654f3f0ac9b0c2ab7
JacekLab <jacek.dynski@gmail.com>
2021-02-21 16:36:54 +0100

    OZEN-1 add loaders to webpack, adjust tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,35 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./out/App.js",
+  entry: "./src/index.ts",
+  mode: "development",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-  externals: {
-    react: "React",
-    "react-dom": "ReactDOM",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)/,
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]",
+          outputPath: "img",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    modules: [path.resolve(__dirname, "./src"), "node_modules"],
   },
 };
